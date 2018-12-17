af6b564fb1d6a5d4dc193ae37a7bddcf2b937810
Chamin Lim <chaminlim@CMLim-MacBook-Pro.local>
2018-12-17 09:43:03 -0500

    Unit04-Lab13 done

--- Unit 1-5/Unit4/Lab13/ScoreCard13.java
+++ Unit 1-5/Unit4/Lab13/ScoreCard13.java
@@ -1,4 +1,4 @@
-   //Name______________________________ Date_____________
+   //Name: Chamin Lim, Date: 12/13/18
    import javax.swing.*;
    import java.awt.*;
    public class ScoreCard13 extends JPanel
@@ -7,12 +7,12 @@
       public ScoreCard13()
       {
          setLayout(new GridLayout(5, 18));
-      
+
          for(int hole = 1; hole <= 18; hole++)
          {
             add(new JLabel("" + hole, SwingConstants.CENTER));
          }
-      
+
          input = new JTextField[4][18];
          for(int r = 0; r < input.length; r++)
             for(int c = 0; c < input[0].length; c++)
@@ -23,42 +23,67 @@
       }
       public void randomize()
       {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
+        for(int r = 0; r < input.length; r++){
+          for(int c = 0; c < input[0].length; c++){
+            input[r][c].setText("" + (int)(Math.random()*4+1));
+          }
+        }
       }
       public int findTotal(int r)
       {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
+        int sum = 0;
+        for(int c = 0; c < input[0].length; c++){
+          sum = sum + Integer.parseInt(input[r][c].getText());
+        }
+        return sum;
       }
       public int findAces(int r)
       {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
+        int count = 0;
+        for(int c = 0; c < input[0].length; c++){
+          if(input[r][c].getText().equals("1"))
+            count++;
+        }
+        return count;
       }
       public int findHardestHole(int r)
       {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
+        int max = Integer.MIN_VALUE;
+        for(int c = 0; c < input[0].length; c++){
+          max = Math.max(max, Integer.parseInt(input[r][c].getText()));
+        }
+        int a = 0;
+        for(int c = 0; c < input[0].length; c++){
+          if(max == Integer.parseInt(input[r][c].getText())){
+            a = c;
+            break;
+          }
+        }
+        return a+1;
       }
       public int findHardestHole()
       {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int hard = 0;
+        for (int c = 0; c < input[0].length; c++) {
+          for(int r = 0; r < input.length; r++){
+            sum = sum + Integer.parseInt(input[r][c].getText());
+            max = Math.max(max, sum);
+          }
+          sum = 0;
+        }
+        int add = 0;
+        for (int c = 0; c < input[0].length; c++) {
+          for(int r = 0; r < input.length; r++){
+            add = add + Integer.parseInt(input[r][c].getText());
+            if(add == max){
+              hard = c;
+              break;
+            }
+          }
+          add = 0;
+        }
+        return hard + 1;
       }
    }
