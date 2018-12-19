0c7590712c5ee337e1969bf6ab02b790122b3118
Chamin Lim <chaminlim@CMLim-MacBook-Pro.local>
2018-12-19 14:13:22 -0500

    Unit4/Lab10 done

--- Unit 1-5/Unit4/Lab10/Display10.java
+++ Unit 1-5/Unit4/Lab10/Display10.java
@@ -1,51 +1,57 @@
-   //Name______________________________ Date_____________
-   import javax.swing.*;
-   import java.awt.*;
-   public class Display10 extends JPanel
-   {
-      private JLabel[] bits;
-      public Display10()
-      {
-         setLayout(new GridLayout(1, 8));
-         bits = new JLabel[8];
-         for(int x = 0; x < bits.length; x++)
-         {
-            bits[x] = new JLabel("", SwingConstants.CENTER);
-            bits[x].setFont(new Font("Serif", Font.BOLD, 50));
-            add(bits[x]);
-         }
-         randomize();
-      }
-      public void randomize()
-      {
-         	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
-      }
-      public void reverse()
-      {
-           	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
-      }
-      public void shift()
-      {
-           	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
-      }
-      public void rotate()
-      {
-           	/************************/
-         	/*                      */
-         	/* Your code goes here. */
-         	/*                      */
-         	/************************/
-      }
-   }
+
+//Name: Chamin Lim, Date: 12/19/18
+import javax.swing.*;
+import java.awt.*;
+
+public class Display10 extends JPanel {
+  private JLabel[] bits;
+
+  public Display10() {
+    setLayout(new GridLayout(1, 8));
+    bits = new JLabel[8];
+    for (int x = 0; x < bits.length; x++) {
+      bits[x] = new JLabel("", SwingConstants.CENTER);
+      bits[x].setFont(new Font("Serif", Font.BOLD, 50));
+      add(bits[x]);
+    }
+    randomize();
+  }
+
+  public void randomize() {
+    for (int x = 0; x < bits.length; x++) {
+      bits[x].setText(String.valueOf((int) (Math.random() * 2)));
+    }
+  }
+
+  public void reverse() {
+    int[] temp = new int[8];
+    for (int x = 0; x < temp.length; x++) {
+      temp[x] = Integer.parseInt(String.valueOf(bits[x].getText()));
+    }
+    for (int x = 0; x < bits.length; x++) {
+      bits[x].setText(String.valueOf(temp[7 - x]));
+    }
+  }
+
+  public void shift() {
+    bits[0].setText(bits[1].getText());
+    bits[1].setText(bits[2].getText());
+    bits[2].setText(bits[3].getText());
+    bits[3].setText(bits[4].getText());
+    bits[4].setText(bits[5].getText());
+    bits[5].setText(bits[6].getText());
+    bits[6].setText(bits[7].getText());
+    bits[7].setText("0");
+  }
+
+  public void rotate() {
+    bits[0].setText(bits[1].getText());
+    bits[1].setText(bits[2].getText());
+    bits[2].setText(bits[3].getText());
+    bits[3].setText(bits[4].getText());
+    bits[4].setText(bits[5].getText());
+    bits[5].setText(bits[6].getText());
+    bits[6].setText(bits[7].getText());
+    bits[7].setText(bits[0].getText());
+  }
+}
--- Unit 1-5/Unit4/Lab10/Panel10.java
+++ Unit 1-5/Unit4/Lab10/Panel10.java
@@ -10,26 +10,26 @@
        public Panel10()
       {
          setLayout(new BorderLayout());
-      
+
          display = new Display10();
          add(display, BorderLayout.CENTER);
-      
+
          JPanel panel = new JPanel();
          panel.setLayout(new FlowLayout());
          add(panel, BorderLayout.SOUTH);
-      
+
          JButton button1 = new JButton("Randomize");
          button1.addActionListener(new Listener1());
          panel.add(button1);
-      
+
          JButton button2 = new JButton("Reverse");
          button2.addActionListener(new Listener2());
          panel.add(button2);
-      
+
          JButton button3 = new JButton("Shift");
          button3.addActionListener(new Listener3());
          panel.add(button3);
-      
+
          JButton button4 = new JButton("Rotate");
          button4.addActionListener(new Listener4());
          panel.add(button4);
@@ -38,28 +38,28 @@
       {
           public void actionPerformed(ActionEvent e)
          {
-            //your code goes here;
+            display.randomize();
          }
       }
        private class Listener2 implements ActionListener
       {
           public void actionPerformed(ActionEvent e)
          {
-             //your code goes here;
+            display.reverse();
          }
       }
        private class Listener3 implements ActionListener
       {
           public void actionPerformed(ActionEvent e)
          {
-            display.shift(); //your code goes here;
+            display.shift();
          }
       }
        private class Listener4 implements ActionListener
       {
           public void actionPerformed(ActionEvent e)
          {
-             //your code goes here;
+            display.rotate();
          }
       }
    }
