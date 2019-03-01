7eaf80091e457ff8f6edb0c2b0fef2bbcbc3d554
chaminlim <chamin1109.lim@gmail.com>
2019-03-01 10:43:12 -0500

    test.java done

--- Test/Test.java
+++ Test/Test.java
@@ -1,5 +1,11 @@
 public class Test {
-  public static void main(String[] args) {
-    
-  }
+    public static void main(String[] args) {
+        String address = "chamin@gmail.com";
+        String myUserName = address.substring(0, address.indexOf("@"));
+        String myHostName = address.substring(address.indexOf("@") + 1, address.indexOf("."));
+        String myExtension = address.substring(address.indexOf(".") + 1);
+        System.out.println(myUserName);
+        System.out.println(myHostName);
+        System.out.println(myExtension);
+    }
 }
