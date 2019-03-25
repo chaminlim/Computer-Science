e67338589edf181ad1991351d23c2fe8295224a7
chaminlim <chaminmir@gmail.com>
2019-03-25 09:04:36 -0400

    Unit5 Lab 13 done

--- Unit 1-5/Unit5/Lab13/Lab13.java
+++ Unit 1-5/Unit5/Lab13/Lab13.java
@@ -1,40 +1,48 @@
-//Name: _______________________  Date: ______________
+//Name: Chamin Lim, Date: 2019-03-21, Period: 2
 
-   import javax.swing.JOptionPane;
-    public class Lab13
-   {
-       public static void main(String[] args)
-      {
-         int number = Integer.parseInt(JOptionPane.showInputDialog("How many disks?"));
-         tower(1, 3, 2, number);
-         System.exit(0);
-      }
-      
-       public static void tower(int start, int finish, int helper, int number)
-      {
-         /***************************/ 
-      	/*                         */
-         /* Your code goes here.    */
-         /*                         */
-         /***************************/
-      }
-   }
-	/**********************************
-      Sample run, n = 3					Sample run, n = 4
-    Move disk 1 from 1 to 3.    	Move disk 1 from 1 to 2.
- 	 Move disk 2 from 1 to 2.   	Move disk 2 from 1 to 3.
-    Move disk 1 from 3 to 2.   	Move disk 1 from 2 to 3.
-	 Move disk 3 from 1 to 3.     Move disk 3 from 1 to 2.
-	 Move disk 1 from 2 to 1.		Move disk 1 from 3 to 1.
-    Move disk 2 from 2 to 3.    	Move disk 2 from 3 to 2.
-	 Move disk 1 from 1 to 3. 		Move disk 1 from 1 to 2.
-	 										Move disk 4 from 1 to 3. 
-											Move disk 1 from 2 to 3.
-											Move disk 2 from 2 to 1.
-											Move disk 1 from 3 to 1.
-   										Move disk 3 from 2 to 3.
-   										Move disk 1 from 1 to 2.
-   										Move disk 2 from 1 to 3.
- 											Move disk 1 from 2 to 3.
-   									
-    ******************************/
+import javax.swing.JOptionPane;
+
+public class Lab13 {
+    public static void main(String[] args) {
+        //int number = Integer.parseInt(JOptionPane.showInputDialog("How many disks?"));
+        tower(1, 3, 2, 3);
+        System.exit(0);
+    }
+
+    public static void tower(int start, int finish, int helper, int number) {
+        if (number == 1) {
+            System.out.println("Move disk 1 from " + start + " to " + finish);
+            return;
+        }
+        tower(start, helper, finish, number - 1);
+        System.out.println("Move disk " + number + " from " + start + " to " + finish);
+        tower(helper, finish, start, number - 1);
+    }
+}
+/**********************************
+ Sample run, n = 3
+ Move disk 1 from 1 to 3.
+ Move disk 2 from 1 to 2.
+ Move disk 1 from 3 to 2.
+ Move disk 3 from 1 to 3.
+ Move disk 1 from 2 to 1.
+ Move disk 2 from 2 to 3.
+ Move disk 1 from 1 to 3.
+
+ Sample run, n = 4
+ Move disk 1 from 1 to 2.
+ Move disk 2 from 1 to 3.
+ Move disk 1 from 2 to 3.
+ Move disk 3 from 1 to 2.
+ Move disk 1 from 3 to 1.
+ Move disk 2 from 3 to 2.
+ Move disk 1 from 1 to 2.
+ Move disk 4 from 1 to 3.
+ Move disk 1 from 2 to 3.
+ Move disk 2 from 2 to 1.
+ Move disk 1 from 3 to 1.
+ Move disk 3 from 2 to 3.
+ Move disk 1 from 1 to 2.
+ Move disk 2 from 1 to 3.
+ Move disk 1 from 2 to 3.
+ ******************************/
